5fa8db654ae12a7da4e413e92d66349a8a8815ca
waycleef <khmalikov@gmail.com>
2022-11-05 14:10:26 +0600

    Initial Commit

---
+++ LogInHomeWork6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LogInHomeWork6
+//
+//  Created by Алишер Маликов on 05.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
