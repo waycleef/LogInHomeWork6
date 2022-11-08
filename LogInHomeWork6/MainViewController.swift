e26c00fdd36189c5cdd876985b2be090d2cb2f53
waycleef <khmalikov@gmail.com>
2022-11-08 18:16:39 +0600

    1. Работа над ошибками

--- LogInHomeWork6/MainViewController.swift
+++ LogInHomeWork6/MainViewController.swift
@@ -12,6 +12,9 @@ class MainViewController: UIViewController {
     @IBOutlet var passwordTF: UITextField!
     @IBOutlet var usernameTX: UITextField!
     
+    private let userName = "User"
+    private let password = "Password"
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let logOutVC = segue.destination as? LogOutViewController else { return }
         logOutVC.loginText = usernameTX.text
@@ -24,25 +27,23 @@ class MainViewController: UIViewController {
     
     @IBAction func unwind(segue: UIStoryboardSegue) {
         guard segue.identifier == "unwindSegue" else { return }
-        guard let _ = segue.source as? LogOutViewController else { return }
         passwordTF.text = ""
         usernameTX.text = ""
     }
     
     @IBAction func logInTappedAction(_ sender: UIButton) {
-        guard let inputText = passwordTF.text, !inputText.isEmpty else {
+        guard let inputText = usernameTX.text, !inputText.isEmpty else {
             showAlert(with: "Invalid User Name or Password!", andMessage: "Please enter correct")
             return
         }
         
-        if passwordTF.text != "Password" && usernameTX.text != "User" {
+        if passwordTF.text != password && usernameTX.text != userName {
             showAlert(with: "Wrong User Name or Password!", andMessage: "Tap button Forgot User Name? and Forgot Password?")
         } else {
             performSegue(withIdentifier: "detailSegue", sender: nil)
         }
-        
-        
     }
+    
     
     @IBAction func forgotUserTapAction() {
         showAlert(with: "Oops!", andMessage: "Your login is User ☺️")
@@ -59,7 +60,6 @@ extension MainViewController {
     private func showAlert(with title: String, andMessage message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default) {_ in
-            self.usernameTX.text = ""
             self.passwordTF.text = ""
         }
         alert.addAction(okAction)
